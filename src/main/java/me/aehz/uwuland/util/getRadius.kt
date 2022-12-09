bb2f8fb52aa306815417b5d698427aa887801f3f
tahmdev <business@tomschuelke.dev>
2022-12-09 16:46:52 +0100

    rename r to radius

--- src/main/java/me/aehz/uwuland/util/getRadius.kt
+++ src/main/java/me/aehz/uwuland/util/getRadius.kt
@@ -3,25 +3,20 @@
 
 package me.aehz.uwuland.util
 
-import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.block.Block
-import org.bukkit.entity.Entity
-import org.bukkit.entity.Mob
-import org.bukkit.entity.Player
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
 
-fun getRadius(location: Location, r: Double): MutableList<Block> {
+
+fun getRadius(location: Location, radius: Double): MutableList<Block> {
     val blocks = mutableListOf<Block>()
 
-    var x = location.x - r
+    var x = location.x - radius
 
-    while (x <= location.x + r) {
-        var y = location.y - r
-        while (y <= location.y + r) {
-            var z = location.z - r
-            while (z <= location.z + r) {
+    while (x <= location.x + radius) {
+        var y = location.y - radius
+        while (y <= location.y + radius) {
+            var z = location.z - radius
+            while (z <= location.z + radius) {
                 blocks.add(Location(location.world, x, y, z).block)
                 z += 1.0
             }
