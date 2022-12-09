2c9500bf0cee14721ecfc40e7c21cc27c3e8ade0
tahmdev <business@tomschuelke.dev>
2022-12-09 16:46:56 +0100

    Create Xray.kt

---
+++ src/main/java/me/aehz/uwuland/listener/perks/Xray.kt
@@ -0,0 +1,66 @@
+package me.aehz.uwuland.listener.perks
+
+import me.aehz.uwuland.Uwuland
+import me.aehz.uwuland.data.PerkOwner
+import me.aehz.uwuland.enums.ListenerType
+import me.aehz.uwuland.interfaces.PerkListener
+import me.aehz.uwuland.managers.EventManager
+import me.aehz.uwuland.util.getRadius
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockDamageEvent
+import org.bukkit.event.player.PlayerMoveEvent
+
+
+class Xray(
+    override val plugin: Uwuland,
+    override var isEnabled: Boolean,
+    override val type: ListenerType,
+    override var perkOwners: MutableList<PerkOwner> = mutableListOf()
+) : PerkListener {
+    override var stg = mutableMapOf<String, String>()
+    private val barrierData = Bukkit.createBlockData(Material.BARRIER)
+    private val positions = mutableMapOf<String, Block>()
+
+    init {
+        stg["range"] = "13.0"
+        Bukkit.getPluginManager().registerEvents(this, plugin)
+        EventManager.register(this, type)
+    }
+
+    @EventHandler
+    fun onMove(e: PlayerMoveEvent) {
+        if (!isEnabled) return
+        if (!hasPerk(e.player)) return
+        if (positions[e.player.name] == e.player.location.block) return
+        val range = stg["range"]!!.toDouble()
+        val r = getRadius(
+            e.player.location,
+            range
+        ).filter { it.type != Material.AIR && it.type != Material.NETHER_PORTAL && it.type != Material.END_PORTAL && it.type != Material.WATER && it.type != Material.LAVA }
+        r.forEach {
+            if (it.location.x == e.player.location.block.x + range
+                || it.location.x == e.player.location.block.x - range
+                || it.location.y == e.player.location.block.y + range
+                || it.location.y == e.player.location.block.y - range
+                || it.location.z == e.player.location.block.z + range
+                || it.location.z == e.player.location.block.z - range
+            ) {
+                e.player.sendBlockChange(it.location, Bukkit.createBlockData(it.type))
+            } else {
+                e.player.sendBlockChange(it.location, barrierData)
+            }
+        }
+
+        positions[e.player.name] = e.player.location.block
+    }
+
+    @EventHandler
+    fun onMine(e: BlockDamageEvent) {
+        if (!isEnabled) return
+        if (!hasPerk(e.player)) return
+        e.player.sendBlockChange(e.block.location, Bukkit.createBlockData(e.block.type))
+    }
+}
