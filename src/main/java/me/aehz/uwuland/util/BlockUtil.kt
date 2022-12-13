44c6c7cab9d02dad929458f2f6133ac24638075f
tahmdev <business@tomschuelke.dev>
2022-12-13 22:07:43 +0100

    Update BlockUtil.kt
    add getBorder

--- src/main/java/me/aehz/uwuland/util/BlockUtil.kt
+++ src/main/java/me/aehz/uwuland/util/BlockUtil.kt
@@ -63,4 +63,33 @@ object BlockUtil {
 
         return blocks
     }
+
+    fun getBorder(block: Block, radius: Double, height: Double = 1.0): MutableList<Block> {
+        val blocks = mutableListOf<Block>()
+        val location = block.location
+
+        var x = location.x - radius
+        while (x <= location.x + radius) {
+            var y = location.y
+            while (y <= location.y + height) {
+                blocks.add(Location(location.world, x, y, location.z - radius).block)
+                blocks.add(Location(location.world, x, y, location.z + radius).block)
+                y += 1.0
+            }
+            x += 1.0
+        }
+
+        var z = location.z - radius
+        while (z <= location.z + radius) {
+            var y = location.y
+            while (y <= location.y + height) {
+                blocks.add(Location(location.world, location.x - radius, y, z).block)
+                blocks.add(Location(location.world, location.x + radius, y, z).block)
+                y += 1.0
+            }
+            z += 1.0
+        }
+
+        return blocks
+    }
 }
